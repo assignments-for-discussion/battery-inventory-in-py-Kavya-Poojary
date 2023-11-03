985524b7ec527c8ea9f74545a9dc2bf94857c418
Kavya-Poojary <149789762+Kavya-Poojary@users.noreply.github.com>
2023-11-03 17:21:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
+def classify_batteries(present_capacity_list):
+    
+    healthy_count = 0
+    exchange_count = 0
+    failed_count = 0
 
-def count_batteries_by_health(present_capacities):
-  return {
-    "healthy": 0,
-    "exchange": 0,
-    "failed": 0
-  }
+    
+    rated_capacity = 120
+
+    for present_capacity in present_capacity_list:
+        
+        soh = (present_capacity / rated_capacity) * 100
+
+        
+        if soh > 80:
+            healthy_count += 1
+        elif 62 <= soh <= 80:
+            exchange_count += 1
+        else:
+            failed_count += 1
+
+    return healthy_count, exchange_count, failed_count
 
 
-def test_bucketing_by_health():
-  print("Counting batteries by SoH...\n")
-  present_capacities = [113, 116, 80, 95, 92, 70]
-  counts = count_batteries_by_health(present_capacities)
-  assert(counts["healthy"] == 2)
-  assert(counts["exchange"] == 3)
-  assert(counts["failed"] == 1)
-  print("Done counting :)")
+def test_classify_batteries():
+   
+    assert classify_batteries([120, 118, 115, 119, 110]) == (5, 0, 0)
+
+    
+    assert classify_batteries([95, 75, 105, 60, 85]) == (2, 2, 1)
+
+   
+    assert classify_batteries([50, 55, 58, 60, 42]) == (0, 0, 5)
+
+    print("All tests pass.")
 
 
-if __name__ == '__main__':
-  test_bucketing_by_health()
+test_classify_batteries()
